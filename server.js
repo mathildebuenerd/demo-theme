fc29e00ec6af00ebf01b962339911a868fe5c6f6
Mathilde Buenerd <mathilde.buenerd@shopify.com>
2023-10-02 13:06:38 +0200

    Add colors settings and use them in the template

--- server.js
+++ server.js
@@ -1,16 +1,21 @@
 const express = require("express");
 const app = express();
 const path = require("path");
+const fs = require("fs");
+const colors = JSON.parse(fs.readFileSync("./settings/colors.json"));
 
 // set the view engine to ejs
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "/"));
 
+// serve up static CSS files in public/assets/ folder when /public link is called in ejs files
+app.use("/public", express.static("public"));
+
 // use res.render to load up an ejs view file
 
 // index page
 app.get("/", function (req, res) {
-  res.render("templates/index");
+  res.render("templates/index", { colors });
 });
 
 // // about page
